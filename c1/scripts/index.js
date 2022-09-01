6a25998ba02bc3987dca7714da5a933527740dd3
Sammyak555 <sammyak.deosale.1@gmail.com>
2022-09-01 09:52:41 +0530

    newc1

---
+++ c1/scripts/index.js
@@ -0,0 +1,18 @@
+/*
+Use local storage as your database. And store data with key as "mobile_data". The mobile object will be :-
+{
+brand: String,
+name: String,
+price: Number,
+image: String
+}
+
+Example:- 
+{
+brand: "apple", (oneplus/nokia)
+name: "Iphone 13",
+price: 74000,
+image: "https://www.iphone.com/image.jpg"
+}
+
+*/
