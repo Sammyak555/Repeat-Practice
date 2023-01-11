49bff72f2e1213c274b5cafff41e6b751155b644
Sammyak555 <sammyak.deosale.1@gmail.com>
2023-01-11 13:21:19 +0530

    acc=rej

--- 6pr-2-del,bookmark/students.js
+++ 6pr-2-del,bookmark/students.js
@@ -1,11 +1,15 @@
 // All the Code for All Students Page Goes Here
 let userarr=JSON.parse(localStorage.getItem("admission"))||[]
+admitarr=JSON.parse(localStorage.getItem("admission-accepted"))||[]
+rejarr=JSON.parse(localStorage.getItem("admission-rejected"))||[]
 let container=document.querySelector("tbody")
+window.addEventListener('load',()=>{
+    displaytable(userarr)
+})
 console.log(userarr)
 function displaytable(){
     container.innerHTML=null;
-    userarr.forEach((el) => {
-        
+    userarr.forEach((el,index) => {
         let tr=document.createElement("tr")
         let td1=document.createElement("td")
         td1.innerText=el.name
@@ -17,8 +21,31 @@ function displaytable(){
         td4.innerText=el.gender
         let td5=document.createElement("td")
         td5.innerText=el.phone
-        tr.append(td1,td2,td3,td4,td5)
+        let adbtn=document.createElement("button")
+        adbtn.innerText="Admitted"
+        adbtn.addEventListener('click',()=>{
+            admitfn(index)
+        })
+        let rejbtn=document.createElement("button")
+        rejbtn.innerText="Rejected"
+        rejbtn.addEventListener('click',()=>{
+            rejectfn(index)
+        })
+        tr.append(td1,td2,td3,td4,td5,adbtn,rejbtn)
         container.append(tr)
     });
 }
-displaytable()
+
+
+function admitfn(index){
+   let user= userarr.splice(index,1)
+    localStorage.setItem("admission-accepted",JSON.stringify(user))
+    localStorage.setItem("admission",JSON.stringify(userarr))
+    window.location.reload()
+}
+function rejectfn(index){
+    let user= userarr.splice(index,1)
+     localStorage.setItem("admission-rejected",JSON.stringify(user))
+     localStorage.setItem("admission",JSON.stringify(userarr))
+     window.location.reload()
+ }
