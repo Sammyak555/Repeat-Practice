5a455fc0a058f63239a161a032156c2c88b6cfb3
Sammyak555 <sammyak.deosale.1@gmail.com>
2023-01-19 22:45:16 +0530

    assa

---
+++ adminFrontend/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Allroutes from './Components/Allroutes';
+
+function App() {
+  return (
+    <div className="App">
+      <Allroutes />
+    </div>
+  );
+}
+
+export default App;
