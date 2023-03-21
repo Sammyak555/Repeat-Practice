7ab5c76cbe6edc14885e1152f3d9ff858d3065c7
Sammyak555 <sammyak.deosale.1@gmail.com>
2023-03-21 16:37:37 +0530

    aa

---
+++ typescript-1/src/index.ts
@@ -0,0 +1,25 @@
+let a:number = 5;
+
+
+
+//boolean
+ let done:boolean = false
+
+ //strings
+
+ let first_name:string = 'sammyak'
+ let last_name:string = 'deosale'
+ 
+type data = {id:number;name:string}
+
+const printing =({id,name}:data):void =>{
+    console.log(`${id},${name}`)
+}
+
+
+
+ let gender : 'male' | 'female' | 'trans' = 'male'
+
+
+//enum
+
